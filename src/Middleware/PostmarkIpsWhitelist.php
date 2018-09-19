72c79ed0be8239ef2e59b97a97233e132bc3923a
Mark van den Broek <mvdnbrk@gmail.com>
2018-09-19 21:57:27 +0200

    Adds middleware to whitelist Postmark IP addresses.

---
+++ src/Middleware/PostmarkIpsWhitelist.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Mvdnbrk\PostmarkWebhooks\Middleware;
+
+use Closure;
+
+class PostmarkIpsWhitelist
+{
+    /**
+     * Array of IP addresses from Postmark that are white listed.
+     * @see https://postmarkapp.com/support/article/800-ips-for-firewalls
+     *
+     * @var array
+     */
+    private $ips = [
+        '50.31.156.6',
+        '50.31.156.77',
+    ];
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (collect($this->ips)->contains($request->getClientIp())) {
+            return $next($request);
+        }
+
+        return response('Unauthorized', 401);
+    }
+}
