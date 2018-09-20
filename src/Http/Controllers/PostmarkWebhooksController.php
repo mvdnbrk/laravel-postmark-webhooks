88185dbcb31b92776bef8e0b915b92182ef2367a
Mark van den Broek <mvdnbrk@gmail.com>
2018-09-20 19:11:10 +0200

    Adds a store method to PostmarkWebhooksController.

--- src/Http/Controllers/PostmarkWebhooksController.php
+++ src/Http/Controllers/PostmarkWebhooksController.php
@@ -2,6 +2,17 @@
 
 namespace Mvdnbrk\PostmarkWebhooks\Http\Controllers;
 
-class PostmarkWebhooksController
+use Illuminate\Routing\Controller;
+
+class PostmarkWebhooksController extends Controller
 {
+    /**
+     * Store the result of a Postmark webhook.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store()
+    {
+        return response()->json()->setStatusCode(202);
+    }
 }
