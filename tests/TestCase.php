ff9c4b59c629a8719671632fa81a6ca17654fc2f
Mark van den Broek <mvdnbrk@gmail.com>
2018-09-20 19:06:01 +0200

    Updates docblock.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,13 +8,9 @@ use Mvdnbrk\PostmarkWebhooks\PostmarkWebhooksServiceProvider;
 abstract class TestCase extends Orchestra
 {
     /**
-     * Get package providers.  At a minimum this is the package being tested, but also
-     * would include packages upon which our package depends, e.g. Cartalyst/Sentry
-     * In a normal app environment these would be added to the 'providers' array in
-     * the config/app.php file.
+     * Get package providers.
      *
      * @param  \Illuminate\Foundation\Application  $app
-     *
      * @return array
      */
     protected function getPackageProviders($app)
