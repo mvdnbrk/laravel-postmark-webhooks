d0089bb342bb3858691d930531a55026fd8b5597
Mark van den Broek <mvdnbrk@gmail.com>
2018-09-21 20:05:40 +0200

    Adds dispatching of an event for a webhook.

--- src/Http/Controllers/PostmarkWebhooksController.php
+++ src/Http/Controllers/PostmarkWebhooksController.php
@@ -38,6 +38,8 @@ class PostmarkWebhooksController extends Controller
             'payload' => $payload->all(),
         ]);
 
+        event('postmark.webhooks:'.snake_case($payload->get('RecordType')), $payload->all());
+
         return response()->json()->setStatusCode(202);
     }
 }
--- tests/Feature/PostmarkWebhooksTest.php
+++ tests/Feature/PostmarkWebhooksTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Illuminate\Support\Facades\Event;
 use Mvdnbrk\PostmarkWebhooks\PostmarkWebhookLog;
 
 class PostmarkWebhooksTest extends TestCase
@@ -11,39 +12,55 @@ class PostmarkWebhooksTest extends TestCase
     {
         parent::setUp();
 
+        Event::fake();
+
         $this->withHeaders(['REMOTE_ADDR' => '50.31.156.6']);
+    }
+
+    protected function validPayload($attributes = [])
+    {
+        return array_merge([
+            'RecordType' => 'SpamComplaint',
+            'MessageID' => '9999-9999-9999-9999-9999',
+        ], $attributes);
     }
 
     /** @test */
     public function it_can_handle_a_valid_request()
     {
         $payload = [
-            'RecordType' => 'SpamComplaint',
-            'MessageID' => '9999-9999-9999-9999-9999',
+            'RecordType' => 'SomeType',
+            'MessageID' => '123456789',
         ];
 
         $response = $this->postJson('/api/webhooks/postmark', $payload);
 
         $response->assertStatus(202);
+
         $this->assertCount(1, PostmarkWebhookLog::all());
+
         tap(PostmarkWebhookLog::first(), function ($log) use ($payload) {
-            $this->assertEquals('9999-9999-9999-9999-9999', $log->message_id);
-            $this->assertEquals('spam_complaint', $log->record_type);
+            $this->assertEquals('123456789', $log->message_id);
+            $this->assertEquals('some_type', $log->record_type);
             $this->assertEquals($payload, $log->payload);
+        });
+
+        Event::assertDispatched('postmark.webhooks:some_type', function ($event, $eventPayload) use ($payload) {
+            return $payload === $eventPayload;
         });
     }
 
     /** @test */
     public function posting_a_valid_request_from_a_non_whitelisted_ip_results_in_a_401()
     {
-        $payload = [
-            'RecordType' => 'SpamComplaint',
-            'MessageID' => '9999-9999-9999-9999-9999',
-        ];
-
-        $response = $this->withHeaders(['REMOTE_ADDR' => '9.9.9.9'])->postJson('/api/webhooks/postmark', $payload);
+        $response = $this->withHeaders(['REMOTE_ADDR' => '9.9.9.9'])->postJson('/api/webhooks/postmark', $this->validPayload([
+            'RecordType' => 'open',
+        ]));
 
         $response->assertStatus(401);
+
         $this->assertCount(0, PostmarkWebhookLog::all());
+
+        Event::assertNotDispatched('postmark.webhooks:open');
     }
 }
