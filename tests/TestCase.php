aec6a50c7a466b04ef4843f480079a6103a30d93
Mark van den Broek <mvdnbrk@gmail.com>
2018-09-21 06:55:21 +0200

    Updates test environment.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,6 +8,32 @@ use Mvdnbrk\PostmarkWebhooks\PostmarkWebhooksServiceProvider;
 abstract class TestCase extends Orchestra
 {
     /**
+     * Setup the test environment.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->artisan('migrate', ['--database' => 'testbench']);
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
+
+    /**
      * Get package providers.
      *
      * @param  \Illuminate\Foundation\Application  $app
