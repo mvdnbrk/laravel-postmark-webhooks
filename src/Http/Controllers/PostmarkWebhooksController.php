7e10515e1ad6aa16991da9364b73a471561c8e7f
Mark van den Broek <mvdnbrk@gmail.com>
2018-09-21 10:03:17 +0200

    Updates test to ensure logs are stored in database.

--- src/Http/Controllers/PostmarkWebhooksController.php
+++ src/Http/Controllers/PostmarkWebhooksController.php
@@ -2,7 +2,9 @@
 
 namespace Mvdnbrk\PostmarkWebhooks\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Mvdnbrk\PostmarkWebhooks\PostmarkWebhookLog;
 use Mvdnbrk\PostmarkWebhooks\Http\Middleware\PostmarkIpsWhitelist;
 
 class PostmarkWebhooksController extends Controller
@@ -23,10 +25,19 @@ class PostmarkWebhooksController extends Controller
     /**
      * Store the result of a Postmark webhook.
      *
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store()
+    public function store(Request $request)
     {
+        $payload = collect($request->input());
+
+        PostmarkWebhookLog::create([
+            'message_id' => $payload->get('MessageID'),
+            'record_type' => snake_case($payload->get('RecordType')),
+            'payload' => $payload->all(),
+        ]);
+
         return response()->json()->setStatusCode(202);
     }
 }
--- tests/Feature/PostmarkWebhooksTest.php
+++ tests/Feature/PostmarkWebhooksTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Mvdnbrk\PostmarkWebhooks\PostmarkWebhookLog;
 
 class PostmarkWebhooksTest extends TestCase
 {
@@ -16,16 +17,33 @@ class PostmarkWebhooksTest extends TestCase
     /** @test */
     public function it_can_handle_a_valid_request()
     {
-        $response = $this->postJson('/api/webhooks/postmark');
+        $payload = [
+            'RecordType' => 'SpamComplaint',
+            'MessageID' => '9999-9999-9999-9999-9999',
+        ];
+
+        $response = $this->postJson('/api/webhooks/postmark', $payload);
 
         $response->assertStatus(202);
+        $this->assertCount(1, PostmarkWebhookLog::all());
+        tap(PostmarkWebhookLog::first(), function ($log) use ($payload) {
+            $this->assertEquals('9999-9999-9999-9999-9999', $log->message_id);
+            $this->assertEquals('spam_complaint', $log->record_type);
+            $this->assertEquals($payload, $log->payload);
+        });
     }
 
     /** @test */
     public function posting_a_valid_request_from_a_non_whitelisted_ip_results_in_a_401()
     {
-        $response = $this->withHeaders(['REMOTE_ADDR' => '9.9.9.9'])->postJson('/api/webhooks/postmark');
+        $payload = [
+            'RecordType' => 'SpamComplaint',
+            'MessageID' => '9999-9999-9999-9999-9999',
+        ];
+
+        $response = $this->withHeaders(['REMOTE_ADDR' => '9.9.9.9'])->postJson('/api/webhooks/postmark', $payload);
 
         $response->assertStatus(401);
+        $this->assertCount(0, PostmarkWebhookLog::all());
     }
 }
