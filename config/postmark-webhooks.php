1cd7b36f27dc14e3fc16742ee6467046f7b8ddbc
Mark van den Broek <mvdnbrk@gmail.com>
2018-09-24 13:54:32 +0200

    Adds doc block.

--- config/postmark-webhooks.php
+++ config/postmark-webhooks.php
@@ -2,6 +2,28 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Log options
+    |--------------------------------------------------------------------------
+    |
+    | Logging events to the database is enabled by default. You may set this
+    | to false if you don't want to log the Postmark events to the database.
+    |
+    | If you wish to use a different table name you spcecify that here as
+    | well.
+    |
+    | If you would like to use your own model you should extend the
+    | Mvdnbrk\PostmarkWebhooks\PostmarkWebhookLog model.
+    |
+    | You may specify one or more event types to be excluded from being
+    | logged to the databse. You can place them under the except key.
+    |
+    | Supported event types: "open", "bounce", "click",
+    | "delivery", "spam_complaint"
+    |
+    */
+
     'log' => [
         'enabled' => env('POSTMARK_WEBHOOKS_LOG_ENABLED', true),
         'table' => 'postmark_webhook_logs',
